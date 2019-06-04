15ebc770179bf461acf2c7e010b58b7052261b63
Daniel Gibbs <daniel.gibbs@verint.com>
2019-06-04 15:23:54 +0100

    before lift arrives scenario running and test passed

--- src/test/java/com/verint/CallingLiftStepdefs.java
+++ src/test/java/com/verint/CallingLiftStepdefs.java
@@ -1,7 +1,9 @@
 package com.verint;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,6 +13,8 @@ public class CallingLiftStepdefs {
 
     private Lift lift;
 
+    private static final boolean OPEN = true;
+    private static final boolean CLOSED = false;
     @Given("^the lift has not yet been called$")
     public void theLiftHasNotYetBeenCalled() {
         lift = new Lift();
@@ -19,5 +23,20 @@ public class CallingLiftStepdefs {
     @Then("the doors are closed")
     public void theDoorsAreClosed() {
         assertThat(lift.areDoorsOpen(), is(equalTo(false)));
+    }
+
+    @Given("the lift is at floor {int}")
+    public void theLiftIsAtFloor(int floorNumber) {
+        lift.setFloorNumber(floorNumber);
+    }
+
+    @When("i call the lift from floor {int}")
+    public void iCallTheLiftFromFloor(int userFloor) {
+        lift.call(userFloor);
+    }
+
+    @Then("the doors are closed before it arrives")
+    public void theDoorsAreClosedBeforeItArrives() {
+        assertThat(lift.previousDoorState(), is(equalTo(CLOSED)));
     }
 }
--- src/test/resources/features/com/verint/CallingTheLift.feature
+++ src/test/resources/features/com/verint/CallingTheLift.feature
@@ -3,3 +3,11 @@ Feature: Scenarios to do with being able to call the lift
   Scenario: Before the lift has been called, the door are shut
     Given the lift has not yet been called
     Then the doors are closed
+
+    Scenario: Before the lift arrives the doors are closed
+      Given the lift has not yet been called
+      And the lift is at floor 3
+      When i call the lift from floor 2
+      Then the doors are closed before it arrives
+
+
